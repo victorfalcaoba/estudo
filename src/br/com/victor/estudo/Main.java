20d7219b0f78642fed4005ccc684d4067ede1777
Victor Falcao <victorfalcao.ba@gmail.com>
2021-02-23 15:58:23 -0300

    Adicionado uma calculadora de somar valores inteiros

--- src/br/com/victor/estudo/Main.java
+++ src/br/com/victor/estudo/Main.java
@@ -1,8 +1,22 @@
 package br.com.victor.estudo;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int x, y;
+
+
+
+		System.out.println("Digite o primeiro valor a ser somado.");
+		x = entrada.nextInt();
+
+		System.out.println("Digite o segundo valor a ser somado.");
+		y = entrada.nextInt();
+
+		System.out.println("O resultado é : " + (x + y));
 	
 	}
 
